9ee292f098ab374e15dfefaf44633a21109846d7
Orhan Keskin <orhankeskinn1@gmail.com>
2023-06-30 22:18:21 +0300

    8. Gün Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,6 +3,7 @@ using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
 ProductManager productManager = new ProductManager(new EfProductDal());
+
 foreach (var product in productManager.GetByUnitPrice(10,100))
 {
     Console.WriteLine(product.ProductName);
