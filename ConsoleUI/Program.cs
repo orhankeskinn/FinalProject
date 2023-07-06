10658f17dfd181824812ced9d69d5e67be9a785c
Orhan Keskin <orhankeskinn1@gmail.com>
2023-07-06 17:33:03 +0300

    9. Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,12 +2,25 @@
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
-ProductManager productManager = new ProductManager(new EfProductDal());
+ProductTest();
+//CategoryTest();
 
-foreach (var product in productManager.GetByUnitPrice(10,100))
+
+static void ProductTest()
 {
-    Console.WriteLine(product.ProductName);
+    ProductManager productManager = new ProductManager(new EfProductDal());
+
+    foreach (var product in productManager.GetProductDetails())
+    {
+        Console.WriteLine(product.ProductName + " kategorisi " + product.CategoryName);
+    }
 }
-{
 
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+    foreach (var category in categoryManager.GetAll())
+    {
+        Console.WriteLine(category.CategoryName);
+    }
 }
