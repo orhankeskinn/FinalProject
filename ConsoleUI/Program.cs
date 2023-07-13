0c7e9b1de46e2b9e54649100023623e4c4280dad
Orhan Keskin <orhankeskinn1@gmail.com>
2023-07-13 19:47:57 +0300

    10. Gün Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,17 +10,26 @@ static void ProductTest()
 {
     ProductManager productManager = new ProductManager(new EfProductDal());
 
-    foreach (var product in productManager.GetProductDetails())
-    {
-        Console.WriteLine(product.ProductName + " kategorisi " + product.CategoryName);
-    }
-}
+    var result = productManager.GetProductDetails();
 
-static void CategoryTest()
-{
-    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-    foreach (var category in categoryManager.GetAll())
+    if (result.Success == true)
     {
-        Console.WriteLine(category.CategoryName);
+        foreach (var product in result.Data)
+        {
+            Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+        }
+    }
+    else
+    {
+        Console.WriteLine(result.Message);
+    }
+
+    static void CategoryTest()
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        foreach (var category in categoryManager.GetAll())
+        {
+            Console.WriteLine(category.CategoryName);
+        }
     }
 }
