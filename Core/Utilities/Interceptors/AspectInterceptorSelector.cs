6647b28e9194002f3b62c50275760da07316b1d7
Orhan Keskin <orhankeskinn1@gmail.com>
2023-07-15 22:14:05 +0300

    12. Gün Ders Sonu

---
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,29 @@
+﻿using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using IInterceptor = Castle.DynamicProxy.IInterceptor;
+
+namespace Core.Utilities.Interceptors
+{
+}
+
+public class AspectInterceptorSelector : IInterceptorSelector
+{
+    public Castle.DynamicProxy.IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+    {
+        var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
+            (true).ToList();
+        var methodAttributes = type.GetMethod(method.Name)
+            .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+        classAttributes.AddRange(methodAttributes);
+        
+
+        return (IInterceptor[])classAttributes.OrderBy(x => x.Priority).ToArray();
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -11,8 +14,13 @@ internal class Program
 
         // Add services to the container.
 
-        builder.Services.AddSingleton<IProductService, ProductManager>();
-        builder.Services.AddSingleton<IProductDal, EfProductDal>();
+        //builder.Services.AddSingleton<IProductService, ProductManager>();
+        //builder.Services.AddSingleton<IProductDal, EfProductDal>();
+        builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+        .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        builder.RegisterModule(new AutofacBusinessModule());
+    });
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
